934d866c5c54d038d5c419b51967f4fab47de044
sebastiian-ab <jhoan.abrahan@campusucc.edu.co>
2026-02-10 21:17:13 +0000

    repaso de variables y datos

---
+++ clase1/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+         //tipos de datos primitivos
+
+         int myInt= 24;
+         System.out.println(myInt);
+
+         double myDouble =2.99;
+         //float,long,byte
+         System.out.println(myDouble);
+
+         char myChar ='a';
+          System.out.println(myChar);
+
+          boolean myBoolean= true;
+          myBoolean=false;
+          System.out.println(myBoolean);
+
+        
+    }
+    
+}
---
+++ clase1/HolaMundoExercises.java
@@ -0,0 +1,29 @@
+public class HolaMundoExercises {
+
+    public static void main(String[] args) {
+        // variables
+
+        String name = "sebastian";
+        System.out.println(name);
+
+        name= "jhoan";
+        System.out.println(name);
+       // name  = 24; no se puede cambiar el tipo de dato
+
+        int age = 24;
+        System.out.println(age);
+       //var se adapta al tipo de variable
+        var email ="jhoansebas1029@gmail.com";
+        System.out.println(email);
+
+        var año=2026;
+        System.out.println(año);
+
+        //constantes
+
+        final String EMAIL = "jhoan.abrahan@campusucc.edu.co";
+        //email = "sebastian@q216gamil.com";no se puede ya definomos la constante
+         System.out.println(EMAIL);
+        
+    }
+}
