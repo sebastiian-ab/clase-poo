312f5012e3027b02ac26b999197e0e4f33a71008
sebastiian-ab <jhoan.abrahan@campusucc.edu.co>
2026-02-11 02:30:43 +0000

    repaso actualizacion

--- clase1/DataTypes.java
+++ clase1/DataTypes.java
@@ -7,6 +7,12 @@ public class DataTypes {
          int myInt= 24;
          System.out.println(myInt);
 
+         long myLong=10;
+
+         //variable tipo real
+         float myFloat=5.0f;
+          System.out.println(myFloat);
+
          double myDouble =2.99;
          //float,long,byte
          System.out.println(myDouble);
@@ -18,7 +24,27 @@ public class DataTypes {
           myBoolean=false;
           System.out.println(myBoolean);
 
+
         
+          // MAS(+) SIGNIFICA CONCATENAR DENTRO DE UN SYSTEM.OUT.PRINTLN
+          System.out.println("la notas es: " + myDouble);
+
+
+          //--------------------------------EXPRESIONES MATEMATICAS-----------------------
+          double a= 4.0;
+          double b = 5.0;
+          double c = 1.0;
+
+
+          double res = b * a - Math.pow(b,2) / 4* c;
+
+          System.out.println(res);
+
+          double res2 = (a*b) / Math.pow(3,2);
+          System.out.println(res2);
+
+          double res3 =(((b + c) / 2 * a + 10) * 3 * b) - 6;
+          System.out.println(res3);
     }
     
 }
