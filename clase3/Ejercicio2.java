a5983b7a508969a283ba0fa155e28e88fcedd861
sebastiian-ab <jhoan.abrahan@campusucc.edu.co>
2026-02-18 01:41:34 +0000

    ejercia realizar

---
+++ clase3/Ejercicio2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc.=new Scanner(System.in);
+
+        double distancia, duracion, precioTotal;
+        System.out.print("ingrese la distancia: ");
+        distancia=sc.nextDouble();
+        System.out.print("ingrese la duracion: ");
+        duracion=sc.nextDouble();
+        
+
+    }
+}
